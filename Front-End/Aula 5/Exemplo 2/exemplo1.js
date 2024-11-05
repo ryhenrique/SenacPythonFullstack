4d3524fadce73764e3603048ae59cb0c086a8c04
gabrielg491 <119019180+ryhenrique@users.noreply.github.com>
2024-11-04 21:55:02 -0300

    Aula 5 HTML + Javascript
    Utilizando JS + Buttons

---
+++ Front-End/Aula 5/Exemplo 2/exemplo1.js
@@ -0,0 +1,39 @@
+alert('Olá')
+console.log('Olá, tudo bem?')
+document.write("Olá terra")
+// alert ('Olá);
+
+// Variáveis var + let + const
+
+var my_var;
+my_var = 'Olá, pessoal';
+alert(my_var)
+// alert(var)
+my_var = 5;
+alert(my_var)
+my_var = [1, 10, 'Olá', true];
+alert(my_var);
+console.log(my_var);
+let exemplo3 = 'Eu também sou uma variável';
+alert(exemplo3);
+document.write("Olá marte")
+document.write('<h1>Eu sou um título</h1>')
+// Operadores Aritméticos
+
+// Basicos: + - * /
+// Resto: %
+// Potenciação: **
+// Incremento e decremento: ++ --
+
+// Operadores Relacionais
+
+// Básicos: < <= > >=
+// Igualdade: == !=
+// Identidade: === !==
+// Propriedade: in
+
+// Operadores Lógicos
+
+// And: &&
+// Or: ||
+// Not: !
---
+++ Front-End/Aula 5/Exemplo 2/exemplo2.js
@@ -0,0 +1,13 @@
+document.write("Olá terra")
+//alert('ola');
+console.log('ola, tudo bem?');
+var variavel1 = 'Ola mundo';
+//alert(variavel1);
+var variavel2 = 123;
+//alert(variavel2)
+console.log(variavel2);
+let variavel3 = 'Eu também sou uma variável';
+//alert(variavel3);
+document.write("Olá marte");
+document.write('<h1> Eu sou um título</h1>');
+document.write('<title> titulo de pagina</title>');
